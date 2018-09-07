9bc1a72dc881840daf662d12baa22ae08ce2065f
Hugo David Farji <hdf1986.6700@gmail.com>
2018-09-06 21:13:41 -0300

    Migrated to rails app with views

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 gem 'rails', '~> 5.1.0'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.7'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
 
 gem 'httparty'
 
@@ -19,12 +21,12 @@ group :development, :test do
   gem 'rubocop', '~> 0.58.2'
   gem 'faker', '~> 1.9.1'
   gem 'factory_bot_rails', '~> 4.11.0'
-
   gem 'webmock', '~> 3.0.1'
 end
 
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
-include WeatherMock
 
 describe WeatherService do
+  include WeatherMock
+
   subject(:service) { described_class.new(3) }
   describe '#weather' do
     before { weather_mock(city, desired_code) }
-  
+
     context 'with a valid city' do
       let(:city) { 'A valid city' }
       let(:desired_code) { 200 }
-      
+
       it 'responds with 200' do
         expect(service.weather(city).code).to be(200)
       end
     end
-  
+
     context 'with a non existing city' do
-  
       let(:city) { 'A non existing city' }
       let(:desired_code) { 404 }
-  
+
       it 'responds with 404' do
         expect(service.weather(city).code).to be(404)
       end
